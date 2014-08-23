ad40be4e9c574be57d286f0f469a651bf98459ca
KaL <Kalson.Kalu@yahoo.com>
2014-08-22 23:45:54 -0400

    update

--- The List/The List/TLAViewController.m
+++ The List/The List/TLAViewController.m
@@ -36,6 +36,7 @@
     niceList.title = @"Nice";
     niceList.tableView.delegate = self;
     niceList.tableView.dataSource = self;
+    niceList.tableView.tag = 0;
     
     [niceList.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
     
@@ -43,6 +44,8 @@
     naughtyList.title = @"Naughty";
     naughtyList.tableView.delegate = self;
     naughtyList.tableView.dataSource = self;
+    naughtyList.tableView.tag = 1;
+
     
     [naughtyList.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
 
@@ -54,12 +57,41 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
+    
+    if (tableView.tag == 0)
+    {
+        return niceArray.count;
+    }
+    
+    if (tableView.tag == 1)
+    {
+        return nauughtArray.count;
+    }
+        
     return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    NSArray *itemArray;
+    // the array is used when it just a simple table view controllers that not clickable
+    
+    if (tableView.tag == 0)
+    {
+         itemArray = niceArray;
+        // the item array is passed to the niceArray
+    }
+    
+    if (tableView.tag == 1)
+    {
+        itemArray = nauughtArray;
+        // the item array is passed to the naughtyArray
+
+    }
+    
+    cell.textLabel.text = itemArray[indexPath.row];
     
     return cell;
 }
