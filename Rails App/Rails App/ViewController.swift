d7f1c549d863f5ba820ce85070741ed69325204e
KaL <Kalson.Kalu@yahoo.com>
2014-09-02 10:48:33 -0400

    update

--- Rails App/Rails App/ViewController.swift
+++ Rails App/Rails App/ViewController.swift
@@ -17,9 +17,58 @@ class ViewController: UIViewController {
     
     @IBAction func postNew(sender: AnyObject) {
         
+        // urlstring connecting to post
+        let myPostURL = API + "posts"
+        
+        // ? -> a query string identifier
+        let newPostURL = API + "posts?post[title]=\(titleField.text)&post[content]=\(contentField.text)"
+        
+        let encodedString = newPostURL.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+        
+        // we want to set a value for the header field
+        var request = NSMutableURLRequest(URL: NSURL(string: encodedString!))
+        
+        request.setValue("zxcasdqwe", forHTTPHeaderField: "AUTH_TOKEN")
+        request.HTTPMethod = "POST"
+        
+        // makes the connection request
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            
+            
+            
+            // converts the json data to object C objects
+            let info: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil)!
+            
+            // ? make the object optional 
+            // an optonal is a wrapper bool value (true or false(bool value), to check if it there or not. if there is a value (its true)
+            // ! pulls it out of the optional
+            
+            println(info)
+            
+        }
     }
     
     @IBAction func getMyPosts(sender: AnyObject) {
+        
+        // urlstring connecting to post
+        let myPostURL = API + "posts"
+        
+        // we want to set a value for the header field
+        var request = NSMutableURLRequest(URL: NSURL(string: myPostURL))
+        
+        // we want to set a value for the header field -> thats why we using a mutable request (to change something on the request)
+        // extra dedfinistion for a rewuest (additonal option to change a request to make more dynamic -> like a note on how u want ur mail deliveres)
+        request.setValue("zxcasdqwe", forHTTPHeaderField: "AUTH_TOKEN")
+        
+        // makes the connection request
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            
+            // converts the json data to object C objects
+            let posts = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSArray
+            
+            println(posts)
+        
+        }
     }
     
     @IBAction func getAllPosts(sender: AnyObject) {
