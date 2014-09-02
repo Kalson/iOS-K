584694db6270e809f65da3e68a601379f8f6a4e0
KaL <Kalson.Kalu@yahoo.com>
2014-09-02 09:49:18 -0400

    json converts request

--- Rails App/Rails App/ViewController.swift
+++ Rails App/Rails App/ViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
+let API = "http://ancient-taiga-9634.herokuapp.com/"
+
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var contentField: UITextField!
+    
+    @IBAction func postNew(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func getMyPosts(sender: AnyObject) {
+    }
+    
+    @IBAction func getAllPosts(sender: AnyObject) {
+        
+        let allPostsURL = API + "posts"
+        
+        // make the request (the actual line making the request)
+        let request = NSURLRequest(URL: NSURL(string: allPostsURL))
+        
+        // makes the connection request
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            
+            // converts the json data to object C objects
+            let posts = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSArray
+            
+            println(posts)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
