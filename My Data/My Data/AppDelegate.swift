b640b6e0becccb2cec0e2160b2be3ca659c57f94
KaL <Kalson.Kalu@yahoo.com>
2014-09-02 11:38:24 -0400

    update

--- My Data/My Data/AppDelegate.swift
+++ My Data/My Data/AppDelegate.swift
@@ -44,7 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.saveContext()
     }
 
-    // MARK: - Core Data stack
+    // MARK: - Core Data Code
 
     lazy var applicationDocumentsDirectory: NSURL = {
         // The directory the application uses to store the Core Data store file. This code uses a directory named "KaL.My_Data" in the application's documents Application Support directory.
@@ -94,6 +94,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     // MARK: - Core Data Saving support
+    
+    // manage object context is the doer
+    // persistentStoreCoordinator holds the stuff
 
     func saveContext () {
         if let moc = self.managedObjectContext {
--- My Data/My Data/ViewController.swift
+++ My Data/My Data/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import CoreData
+
+// Core data loads the cache of the phone (especically essential if u have no wifi)
+
+// with core data you limit the query
 
 class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var appD = UIApplication.sharedApplication().delegate as AppDelegate
+        
+        var fetchRequest = NSFetchRequest()
+        
+        // entity (is like a data table) holds all your data
+        var entity = NSEntityDescription.entityForName("Post", inManagedObjectContext: appD.managedObjectContext)
+        
+        fetchRequest.entity = entity
+
+        var predicate = NSPredicate(format: "user = 'jo@theironyard.com'")
+        
+        fetchRequest.predicate = predicate
+        
+        var sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
+        
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // fetching some amount of data
+        var fetchedObjects = appD.managedObjectContext!.executeFetchRequest(fetchRequest, error: nil)
+    
+        println(fetchedObjects)
+        
     }
 
     override func didReceiveMemoryWarning() {
