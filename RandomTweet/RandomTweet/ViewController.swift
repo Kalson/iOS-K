d9419100551d0885ae0c8a52ecd1d13348ae7988
KaL <Kalson.Kalu@yahoo.com>
2014-09-16 11:23:06 -0400

    loop thru twitter accs

--- RandomTweet/RandomTweet/ViewController.swift
+++ RandomTweet/RandomTweet/ViewController.swift
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import Accounts
+import Social
 import SwifteriOS // running the import tries to build that target
 
 class ViewController: UIViewController {
+    
+    var swifter = Swifter(consumerKey: "", consumerSecret: "")
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // trying to connect to the twiter accounts of the iPhone
+        let accountStore = ACAccountStore()
+        let accountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+        
+        accountStore.requestAccessToAccountsWithType(accountType, options: nil) { (granted: Bool, error: NSError!) -> Void in
+        
+            if granted {
+                let twitterAccounts = accountStore.accountsWithAccountType(accountType)
+                
+                let twitterAccount = twitterAccounts[0] as ACAccount
+                
+                self.swifter = Swifter(account: twitterAccount)
+        
+                println(twitterAccounts)
+                
+                for tAccount in twitterAccounts {
+                    let tA = tAccount as ACAccount
+                    
+//                    println(tA.userFullName)
+                    println(tA.username) // kalsonkalu
+                    println(tA.accountDescription) // @kalsonkalu
+//                    println(tA.credential)
+                    println(tA.identifier) // token
+
+                    
+                    
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
